d7c55a57e760834d08faee67965e16518fec2833
Marcel Sussai de Oliveira <marcelsussaideoliveira@hotmail.com>
2023-02-25 19:24:52 -0300

    Config Emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { GlobalStyles } from '@/theme/GlobalStyle'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
